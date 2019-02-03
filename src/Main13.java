bc2f6c83b543c1353aa91625cb091a333ded7cc8
ljp <ljp@ifourthwall.com>
2019-02-03 11:28:25 +0800

    顺时针矩阵

--- src/Main13.java
+++ src/Main13.java
@@ -6,24 +6,55 @@ public class Main13 {
         int [][] ans=new int[n][n];
         int i = 1;
         int x=0,y=0;
+        int dir=1;
         while (i <= n * n) {
-            ans[y][x]=i;
-            if(x+1<n&&ans[x+1][y]==0&&y+1<=n/2){
-                x++;
-            }else if(y+1<n&&ans[x][y+1]==0&&x+1>n/2){
-                y++;
-            }else if((x>0&&ans[x-1][y]==0)&&y+1>n/2){
-                x--;
-            }else if(y>0||ans[x][y-1]==0){
-                y--;
+            if(i!=1){
+                if(!judgeNextRange(x,y,n,dir,ans)){
+                    dir++;
+                }
+                if(dir%4==1){
+                    x++;
+                }else if(dir%4==2){
+                    y++;
+                }else if(dir%4==3){
+                    x--;
+                }else if(dir%4==0){
+                    y--;
+                }
             }
+            ans[y][x]=i;
             i++;
         }
         return ans;
     }
 
+    private static boolean judgeNextRange(int x, int y, int n, int dir,int[][] ans) {
+        dir=dir%4;
+        if(dir==1){
+            if(x+1>=n||ans[x+1][y]!=0){
+                return false;
+            }
+        }
+        if(dir==2){
+            if(y+1>=n||ans[x][y+1]!=0){
+                return false;
+            }
+        }
+        if(dir==3){
+            if(x==0||ans[y][x-1]!=0){
+                return false;
+            }
+        }
+        if(dir==0){
+            if(y==0||ans[y-1][x]!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
-        generateMatrix(5);
+        generateMatrix(6);
         System.out.print(1);
     }
 
