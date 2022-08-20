84c3dec662a626707f52ae57b58555ec26475a72
FAbyss <ha998322@gmail.com>
2022-08-20 15:48:59 +0800

    123

---
+++ src/Continents.java
@@ -0,0 +1,32 @@
+public class Continents {
+    public static void main(String[] args) {
+        int continent = 4;
+        switch (continent) {
+            case continent = 1:
+                System.out.println("North America: Mexico City, Mexico");
+
+            case continent = 2:
+                System.out.println("South America: Sao Paulo, Brazil");
+
+            case continent = 3:
+                System.out.println("Europe: Moscow, Russia");
+
+            case continent = 4:
+                System.out.println("Africa: Lagos, Nigeria");
+
+            case continent = 5:
+                System.out.println("Asia: Shanghai, China");
+
+            case continent = 6:
+                System.out.println("Australia: Sydney, Australia");
+
+            case continent = 7:
+                System.out.println("Antarctica: McMurdo Station, US");
+
+            default{System.out.println("Undefined continent!");}
+        }
+
+
+
+    }
+}
