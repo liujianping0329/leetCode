44832d168ed7c45dbe562104372e8830d0a86af6
FAbyssSS <91474576+FAbyssSS@users.noreply.github.com>
2023-04-22 10:11:11 +0900

    20230422

--- src/Main_rennsyu.java
+++ src/Main_rennsyu.java
@@ -2,11 +2,14 @@ public class Main_rennsyu {
     public boolean isPalindrome(int x) {
         String string = x+"";
         String string1 = new StringBuffer(string).reverse().toString();
-        return string1.equals(string);
+        if (string1.equals(string)){
+            return true;
+        }
+        return false;
     }
     public static void main(String[] args) {
-        Main_rennsyu main50=new Main_rennsyu();
-        boolean res = main50.isPalindrome(12231);
-        System.out.print(res);
+        Main_rennsyu main51=new Main_rennsyu();
+        boolean res = main51.isPalindrome(121);
+            System.out.print(res);
     }
 }
